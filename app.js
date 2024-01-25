1209c137e3b1faea256ca0ad808c863979eb96a6
hassantahir7 <133124489+hassantahir7@users.noreply.github.com>
2024-01-25 17:38:46 +0500

    ENV ENVIRONMENT

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import path from 'path'; // Add this line for path module
-import { fileURLToPath } from 'url'; // Add this line for fileURLToPath
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { submitForm } from './controllers/formController.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://root1:1234@simpleapp.u9lwvxg.mongodb.net/your-database-name', {
+// Connect to MongoDB Atlas using environment variable
+const mongoURI = process.env.MONGODB_URI; // Replace with your local MongoDB URI
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB Atlas:', err);
-    });
+  .then(() => {
+      console.log('Connected to MongoDB Atlas');
+  })
+  .catch((err) => {
+      console.error('Error connecting to MongoDB Atlas:', err);
+  });
 
 // Middleware to parse incoming request data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve the HTML form
-const __filename = fileURLToPath(import.meta.url); // Add this line for __filename
-const __dirname = path.dirname(__filename); // Add this line for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Handle form submissions
@@ -37,6 +38,5 @@ app.post('/submit-form', submitForm);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
